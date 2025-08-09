5cb33257dfff1b4cabcbe53d5cfe0417c68774eb
kimhyojung <706shin1728@naver.com>
2025-08-09 13:45:51 +0900

    fix: remove generic type parameter from isPlainArray function

--- packages/persister/src/compare.ts
+++ packages/persister/src/compare.ts
@@ -62,8 +62,7 @@ export function shallowEqualObjects<T extends Record<string, any>>(
 
   return true
 }
-
-export function isPlainArray<T>(value: unknown): value is Array<T> {
+export function isPlainArray(value: unknown): value is Array<any> {
   return Array.isArray(value) && value.length === Object.keys(value).length
 }
 
